db3db059d8dba5d41dcae7110e6febcad9ea8f87
Ted Bell <tchbell@gmail.com>
2018-03-08 15:38:44 -0500

    Created weather component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from  '@angular/platform-browser';
 
 import{ AppComponent } from './app.component';
+import { WeatherComponent } from './weather-widget/component/weather.component';
+
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    WeatherComponent
+    ],
   bootstrap: [AppComponent]
 })
 
