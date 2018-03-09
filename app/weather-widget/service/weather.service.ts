0d025de36cfd87c050ebb6477325a637db259ef9
Ted Bell <tchbell@gmail.com>
2018-03-09 13:31:55 -0500

    got geolocation working

--- app/weather-widget/service/weather.service.ts
+++ app/weather-widget/service/weather.service.ts
@@ -2,4 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class WeatherService{
+  getCurrentLocation(): [number, number]{
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(pos => {
+        console.log("Position: " + pos.coords.latitude, "," , pos.coords.longitude);//TODO REMOVE
+        return [pos.coords.latitude, pos.coords.longitude];
+      },
+      err => console.error("Unable to get the location"))
+    }else{
+      console.error("Geolocation is not available");
+      return [0,0];
+    }
+  }
 }
